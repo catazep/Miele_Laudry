a8ef715ef6a898d103c054edaae184e005cc4a1f
Georgescu Catalin <catazep@yahoo.com>
2026-04-20 15:40:19 +0300

    Basic navbar

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { AuthService } from '../../../core/services/auth.service';
+import { ROUTES } from '../../../core/constants/routes';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +10,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./nav.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink],
 })
-export class NavComponent {}
+export class NavComponent {
+  private readonly authService = inject(AuthService);
+
+  public readonly routes = ROUTES;
+  public readonly currentUser = this.authService.currentUser;
+
+  public logout(): void {
+    this.authService.logout();
+  }
+}
