227505e3ff6a6c8986a2f3c5342b1b7548611866
Georgescu Catalin <catazep@yahoo.com>
2026-04-20 14:40:54 +0300

    Miele fonts+icons + font awesome
    - add template nav
    - add miele's fonts
    - add miele's icons
    - add font awesome

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { NavComponent } from './shared/components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {}
